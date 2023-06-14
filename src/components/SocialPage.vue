b18062c2b48ef6fa3122a66c61de5e815409e518
Fernando Miranda A <artunduaga74@gmail.com>
2023-06-14 00:24:57 -0500

    social page ciclo for

--- src/components/SocialPage.vue
+++ src/components/SocialPage.vue
@@ -1,47 +1,25 @@
 <template>
-<v-container
-fluid
-  class=""
->
- <v-row dense class="align-center justify-center">
-   <v-col cols="sx-6 sm-auto" class="">
-    <v-img
-                cover
-                class="elevation-2"
-                src= "/src/assets/m1.jpg"
-              >
-            </v-img>
-   </v-col>
-   <v-col cols="sx-6 sm-auto" class="">
-    <v-img
-              cover
-                class="elevation-2"
-                src= "/src/assets/m3.jpg"
-              >
-            </v-img>
-   </v-col>
- </v-row>
- <v-row  dense class="align-center justify-center">
-   <v-col cols="sx-6 sm-auto" class="">
-    <v-img
-                cover=""
-                class="elevation-2"
-                src= "/src/assets/m2.jpg"
-              >
-            </v-img>
-   </v-col>
-   <v-col cols="sx-6 sm-auto" class="c">
-    <v-img
-             
-             cover
-                class="elevation-2"
-                src= "@/assets/m4.jpg"
-              >
-            </v-img>
-   </v-col>
- </v-row>
-</v-container>
+  <v-container fluid class="">
+    <v-row dense class="align-center">
+      <v-col cols="6" class="" v-for="imagen in imagenes">
+        <v-img cover class="elevation-2" :src="getImageUrl(imagen)">
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-<script setup>
-
+<script>
+export default {
+  computed: {
+    //
+  },
+  methods: {
+    getImageUrl(imagen) {
+      return `/src/assets/${imagen}`;
+    },
+  },
+  data: () => ({
+    imagenes: ["m1.jpg", "m3.jpg", "m2.jpg", "m4.jpg"],
+  }),
+};
 </script>
