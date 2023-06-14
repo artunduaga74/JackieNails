02d122430b37178d12e47ddc1d17dfe438ea1c7f
Fernando Miranda A <artunduaga74@gmail.com>
2023-06-14 00:11:47 -0500

    social page original

--- src/components/SocialPage.vue
+++ src/components/SocialPage.vue
@@ -3,48 +3,45 @@
 fluid
   class=""
 >
- <v-row dense class="align-center justify-center"
- v-for= "(imagen, index) in imagenes"
-  :key="index" 
- >
+ <v-row dense class="align-center justify-center">
    <v-col cols="sx-6 sm-auto" class="">
     <v-img
-               v-if="(index) % 2 == 0"
                 cover
                 class="elevation-2"
-                :src="getImageUrl(imagenes[index])"
-    >
-  </v-img>
+                src= "/src/assets/m1.jpg"
+              >
+            </v-img>
    </v-col>
    <v-col cols="sx-6 sm-auto" class="">
     <v-img
-                v-if="(index) % 2 !== 0"           
-                cover
+              cover
                 class="elevation-2"
-                :src="getImageUrl(imagenes[index])"
-    >
-  </v-img>
+                src= "/src/assets/m3.jpg"
+              >
+            </v-img>
+   </v-col>
+ </v-row>
+ <v-row  dense class="align-center justify-center">
+   <v-col cols="sx-6 sm-auto" class="">
+    <v-img
+                cover=""
+                class="elevation-2"
+                src= "/src/assets/m2.jpg"
+              >
+            </v-img>
+   </v-col>
+   <v-col cols="sx-6 sm-auto" class="c">
+    <v-img
+             
+             cover
+                class="elevation-2"
+                src= "@/assets/m4.jpg"
+              >
+            </v-img>
    </v-col>
  </v-row>
 </v-container>
 </template>
-<script>export default {
-  computed: {
-    //
-  },
-  methods: {
-    getImageUrl(imagen) {
-        
-      return `/src/assets/${imagen}`
-    },
-  },
-  data: () => ({
-    imagenes: [
-      'm1.jpg',
-      'm2.jpg',
-      'm3.jpg',
-      'm4.jpg'
-    ]
-  })
-};
+<script setup>
+
 </script>
